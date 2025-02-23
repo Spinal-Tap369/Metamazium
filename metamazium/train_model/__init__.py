f1604b080b38951978d47a9aef6badcf1f5f60ce
spinalTap369 <samvd123@gmail.com>
2025-02-23 13:19:55 +0100

    adding lstm based supervised meta learner

--- metamazium/train_model/__init__.py
+++ metamazium/train_model/__init__.py
@@ -3,10 +3,12 @@
 from .train_lstm_trpo_fo import main as train_lstm_trpo_fo_main
 from .train_snail_trpo_fo import main as train_snail_trpo_fo_main
 from .train_snail import main as train_snail_main
-# from .train_snail_m import main as train_snail_m_main
+from .train_lstm_svl import main as train_lstm_svl_main
+
 __all__ = [
     "train_lstm_trpo_fo_main",
     "train_snail_trpo_fo_main",
     "train_snail_main",
-    # "train_snail_m_main",
+    "train_lstm_svl_main",
+
 ]
